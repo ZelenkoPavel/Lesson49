14c7ebf7280ac7c99b5be7fc1c6a57a9f9566d15
ITSTEP\st <st>
2024-03-14 18:46:56 +0300

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Plane {
+public:
+	string name;
+	int speed;
+	double length;
+	double height;
+	int count;
+
+};
+
+int main() {
+	int n;
+	n = 10;
+
+	Plane plane1;
+
+	plane1.name = "boing747-400";
+	plane1.speed = 950;
+}
