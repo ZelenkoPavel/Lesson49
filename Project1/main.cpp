ad221e20e4b38c1f4553e0478dcd25412f8647cb
ITSTEP\st <st>
2024-03-14 18:50:30 +0300

    .

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -16,8 +16,16 @@ int main() {
 	int n;
 	n = 10;
 
-	Plane plane1;
+	Plane plane1, plane2;
+	const int size = 5;
+	Plane planes[size];
+
+	planes[0].name = "";
+
 
 	plane1.name = "boing747-400";
 	plane1.speed = 950;
+	plane1.length = 70.;
+	plane1.height = 19.41;
+	plane1.count = 700;
 }
